7186f7288fc2ea45aded6d99f8fc489a7df21cf3
azizikri <azizikri@gmail.com>
2022-08-06 02:50:52 +0700

    feat: add depedencies

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,34 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import Editor from '@toast-ui/editor'
+import '@toast-ui/editor/dist/toastui-editor.css';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+const editor = new Editor({
+  el: document.querySelector('#editor'),
+  height: '400px',
+  initialEditType: 'markdown',
+  placeholder: 'Write something cool!',
+})
+
+if (document.querySelector('#createPostForm')) {
+    document.querySelector('#createPostForm').addEventListener('submit', e => {
+        e.preventDefault();
+        document.querySelector('#content').value = editor.getMarkdown();
+        e.target.submit();
+    });
+}
+
+if (document.querySelector('#editPostForm')) {
+    editor.setMarkdown(document.querySelector('#oldContent').value);
+
+    document.querySelector('#editPostForm').addEventListener('submit', e => {
+        e.preventDefault();
+        document.querySelector('#content').value = editor.getMarkdown();
+        e.target.submit();
+    });
+}
