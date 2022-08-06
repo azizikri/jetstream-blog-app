b365864fb97a24a84cf2d42eecd597bcb722b9d5
azizikri <azizikri@gmail.com>
2022-08-06 23:19:09 +0700

    refactor: showng post content with str markdown

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,15 +2,23 @@
     <div class="py-5">
         <div class="container max-w-2xl mx-auto sm:px-6 lg:px-8">
             <h1 class="antialiased text-2xl">{{ $post->title }}</h1>
-            <small>{{ \Carbon\Carbon::parse($post->updated_at)->format('jS M, Y') }} on <a href="{{ route('posts.user.show', $post->user->username) }}"
-                    class="text-blue-600">{{ $post->user->username }}'s blog</a></small>
+            <div class="flex justify-between">
+                <small>{{ \Carbon\Carbon::parse($post->updated_at)->format('jS M, Y') }} on <a
+                        href="{{ route('posts.user.show', $post->user->username) }}"
+                        class="text-blue-600">{{ $post->user->username }}'s blog</a></small>
+                <div>
+                    <a href="{{ route('posts.edit', $post->slug) }}" class="text-blue-600 px-3">Edit</a>
+                    <form action="{{ route('posts.destroy', $post->slug) }}" method="POST" class="inline">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="text-red-600"
+                            onclick="return confirm('Are you sure?')">Delete</button>
+                    </form>
+                </div>
+            </div>
             <hr>
-            <div class="py-5">
-                {{-- <p class="text-base break-all font-light leading-relaxed mt-0 mb-4 text-gray-800"> --}}
-                    <x-markdown class="break-all">
-                        {{ $post->content }}
-                    </x-markdown>
-                {{-- </p> --}}
+            <div class="py-5 break-all">
+                {!! \Illuminate\Support\Str::markdown($post->content) !!}
             </div>
         </div>
 </x-app-layout>
