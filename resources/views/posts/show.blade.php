2fb7f06f3a63d2106d221478a2c0c971b6fddec7
azizikri <azizikri@gmail.com>
2022-08-06 22:59:24 +0700

    refactor: change carbon parse format

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,7 +2,7 @@
     <div class="py-5">
         <div class="container max-w-2xl mx-auto sm:px-6 lg:px-8">
             <h1 class="antialiased text-2xl">{{ $post->title }}</h1>
-            <small>{{ \Carbon\Carbon::parse($post->updated_at)->format('jS M, Y') }} on <a href=""
+            <small>{{ \Carbon\Carbon::parse($post->updated_at)->format('jS M, Y') }} on <a href="{{ route('posts.user.show', $post->user->username) }}"
                     class="text-blue-600">{{ $post->user->username }}'s blog</a></small>
             <hr>
             <div class="py-5">
