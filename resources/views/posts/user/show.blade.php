3f2bc10d37815f7a3bb88b0353eaf9b11c430d79
azizikri <azizikri@gmail.com>
2022-08-06 22:59:44 +0700

    feat: add view user posts

---
+++ resources/views/posts/user/show.blade.php
@@ -0,0 +1,25 @@
+<x-app-layout>
+    <div class="pt-10">
+        <h1 class="antialiased text-center text-2xl">{{ $user->username }}'s Blog</h1>
+    </div>
+
+    <div class="py-5">
+        <div class="max-w-2xl mx-auto sm:px-6 md:px-6 lg:px-8">
+            <hr>
+            <div class="relative overflow-x-auto py-5">
+                @forelse ($posts as $post)
+                    <div class="flex py-2">
+                        <div>
+                            <small>{{ \Carbon\Carbon::parse($post->updated_at)->format('jS M, Y') }}</small>
+                            <a href="{{ route('posts.show', [$post->user->username, $post->slug]) }}" class="text-blue-600 break-all">
+                                <p>{{ $post->title }}</p>
+                            </a>
+                        </div>
+                    </div>
+                @empty
+                    <p class="text-center">No posts yet.</p>
+                @endforelse
+            </div>
+        </div>
+    </div>
+</x-app-layout>
